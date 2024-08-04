9641df43daf53fdaf14b4adfbc3239baffaf680b
Anurag <dev.anuragkr@gmail.com>
2024-08-04 21:12:26 +0530

    added input component

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,22 +1,25 @@
-function Input({
-  type,
-  placeholder,
-  id,
-  register,
-  autoComplete,
-  defaultValue,
-}) {
-  return (
-    <input
-      {...register}
-      id={id}
-      placeholder={placeholder}
-      type={type}
-      defaultValue={defaultValue}
-      autoComplete={autoComplete}
-      className="bg-transparent text-grey-dark outline-none"
-    />
-  );
-}
+import * as React from 'react';
+
+import { cn } from '../util/cn';
+
+const Input = React.forwardRef(
+  ({ className, type, invalid, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          'border-border flex h-12 w-full rounded-lg border bg-transparent pl-11 pr-4 text-base text-grey-dark outline-none transition-colors',
+          invalid
+            ? ' focus-visible:shadow-error border-red'
+            : 'focus-visible:border-purple focus-visible:shadow-input',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = 'Input';
 
 export default Input;
