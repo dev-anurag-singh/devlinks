aa1c867281dd226b4b85e574ea54e20d677b08f4
Anurag <dev.anuragkr@gmail.com>
2024-08-04 21:13:10 +0530

    signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import SignupForm from '../features/auth/SignupForm';
+
+function Signup() {
+  return (
+    <>
+      <div className="mb-10">
+        <h4 className="mb-2 text-md font-bold text-grey-dark md:text-heading">
+          Create Account
+        </h4>
+        <p>Let`&apos;`s get you started sharing your links!</p>
+      </div>
+      <SignupForm />
+      <div className="mt-6 justify-center gap-1 text-center md:flex">
+        <p>Already have an account?</p>
+        <Link to="/auth/login" className="text-purple">
+          Login
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Signup;
