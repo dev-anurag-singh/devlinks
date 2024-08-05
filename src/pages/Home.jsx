bf86db40f53c76a5b31ba51cf1ae4e1f071e9fb8
Anurag <dev.anuragkr@gmail.com>
2024-08-05 23:26:57 +0530

    Dashboard page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import Links from '../features/links/Links';
+import LinksForm from '../features/links/LinksForm';
 
 function Home() {
   return (
-    <>
-      <Links />
-    </>
+    <div className="space-y-10">
+      <div className="flex flex-col gap-2 p-6 pb-0 md:p-10 md:pb-0">
+        <h2 className="text-md font-bold text-grey-dark md:text-heading">
+          Customize your links
+        </h2>
+        <p className="">
+          Add/edit & remove links below and then share all your profiles with
+          the world!
+        </p>
+      </div>
+      <LinksForm />
+    </div>
   );
 }
 
