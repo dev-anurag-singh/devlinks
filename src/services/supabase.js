cffd85d6e2dc3db3897b30e9cce8dbf5786fb0f7
Anurag <dev.anuragkr@gmail.com>
2025-03-08 15:04:37 +0530

    created env variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://xhzwkxqtgpkrgspmmfbx.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoendreHF0Z3BrcmdzcG1tZmJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5MzEyODcsImV4cCI6MjAxMjUwNzI4N30.YEsz5oY0cgyfQiMIOFB62nPJtcwSR7MDCJt2eYxI23w';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
